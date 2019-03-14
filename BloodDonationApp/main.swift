7119142f386db220996744f5a5222a8501c5db37
alebing <alexandrebing@gmail.com>
2019-03-14 08:35:37 -0300

    Initial Commit

---
+++ BloodDonationApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BloodDonationApp
+//
+//  Created by Alexandre Scheer Bing on 14/03/19.
+//  Copyright © 2019 Alexandre Scheer Bing. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
